56a286ed04ad113a00081f8e83550c2ef6122488
krasimirr <krusev0601@gmail.com>
2016-02-28 08:43:09 +0000

    Update: Debug false, no admin rights

--- CDX_project/CDX_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from cdxapp import views
-
-urlpatterns = patterns('',
-    url(r'^admin/', views.admin, name='admin'),                       
-    url(r'^cdxapp/', include('cdxapp.urls')),
-    url(r'^$', include('cdxapp.urls')),)
--- CDX_project/cdxapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url, include
-from cdxapp import views
-from django.contrib import admin
-
-urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'),
-	url(r'^login', views.login, name='login'),
-	url(r'^logout', views.logout, name='logout'),
-        url(r'^about/', include(admin.site.urls)))
